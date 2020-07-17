f9f857abbbf3752458fc2ddeb2f67780bca8a590
SunQiXin <15153869872@163.com>
2020-07-17 16:48:16 +0800

    完成品牌管理功能

--- renren-fast-vue/src/utils/index.js
+++ renren-fast-vue/src/utils/index.js
@@ -1,22 +1,33 @@
-import Vue from 'vue'
-import router from '@/router'
-import store from '@/store'
+/*
+ * @description:
+ * @AUthor: 孙启新
+ * @Date: 2020-07-11 14:16:09
+ * @LastEditTime: 2020-07-17 09:15:42
+ * @FilePath: \gulimall-view\renren-fast-vue\src\utils\index.js
+ */
+
+import Vue from "vue";
+import router from "@/router";
+import store from "@/store";
 
 /**
  * 获取uuid
  */
-export function getUUID () {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    return (c === 'x' ? (Math.random() * 16 | 0) : ('r&0x3' | '0x8')).toString(16)
-  })
+export function getUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    return (c === "x" ? (Math.random() * 16) | 0 : "r&0x3" | "0x8").toString(
+      16
+    );
+  });
 }
 
 /**
  * 是否有权限
  * @param {*} key
  */
-export function isAuth (key) {
-  return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+export function isAuth(key) {
+  // return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+  return true;
 }
 
 /**
@@ -25,34 +36,34 @@ export function isAuth (key) {
  * @param {*} id
  * @param {*} pid
  */
-export function treeDataTranslate (data, id = 'id', pid = 'parentId') {
-  var res = []
-  var temp = {}
+export function treeDataTranslate(data, id = "id", pid = "parentId") {
+  var res = [];
+  var temp = {};
   for (var i = 0; i < data.length; i++) {
-    temp[data[i][id]] = data[i]
+    temp[data[i][id]] = data[i];
   }
   for (var k = 0; k < data.length; k++) {
     if (temp[data[k][pid]] && data[k][id] !== data[k][pid]) {
-      if (!temp[data[k][pid]]['children']) {
-        temp[data[k][pid]]['children'] = []
+      if (!temp[data[k][pid]]["children"]) {
+        temp[data[k][pid]]["children"] = [];
       }
-      if (!temp[data[k][pid]]['_level']) {
-        temp[data[k][pid]]['_level'] = 1
+      if (!temp[data[k][pid]]["_level"]) {
+        temp[data[k][pid]]["_level"] = 1;
       }
-      data[k]['_level'] = temp[data[k][pid]]._level + 1
-      temp[data[k][pid]]['children'].push(data[k])
+      data[k]["_level"] = temp[data[k][pid]]._level + 1;
+      temp[data[k][pid]]["children"].push(data[k]);
     } else {
-      res.push(data[k])
+      res.push(data[k]);
     }
   }
-  return res
+  return res;
 }
 
 /**
  * 清除登录信息
  */
-export function clearLoginInfo () {
-  Vue.cookie.delete('token')
-  store.commit('resetStore')
-  router.options.isAddDynamicMenuRoutes = false
+export function clearLoginInfo() {
+  Vue.cookie.delete("token");
+  store.commit("resetStore");
+  router.options.isAddDynamicMenuRoutes = false;
 }
